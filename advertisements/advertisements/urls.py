094441c28564fe8703f5ccd7d636af85806c6da8
Владимир <vrprudnikov@gmail.com>
2023-08-07 15:26:42 +0300

    new files

--- advertisements/advertisements/urls.py
+++ advertisements/advertisements/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_advertisements.urls'))
+    path('', include('app_advertisements.urls')),
+    path('lesson_4', include('app_lesson.urls'))
 ]
